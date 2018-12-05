c7d53d7f955834cab149bac06a4bee2d2a767421
pantheonsh <pantheonshell@yahoo.com>
2018-12-05 17:07:58 -0200

    Moedex loader

---
+++ moedex.go
@@ -0,0 +1,34 @@
+package moedex
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type Moemon struct {
+	Id         int      `json:"id"`
+	Number     string   `json:"num"`
+	Name       string   `json:"name"`
+	Type       []string `json:"type"`
+	Weaknesses []string `json:"weaknesses"`
+}
+
+// Moemons Moemon data
+var (
+	Moemons []Moemon
+)
+
+func Load() {
+	b, err := ioutil.ReadFile("assets/pokedex.json")
+
+	if err != nil {
+		log.Fatalln("Failed to load Pokedex data", err)
+	}
+
+	err = json.Unmarshal(b, &Moemons)
+
+	if err != nil {
+		log.Fatalln("Failed to parse Pokedex data", err)
+	}
+}
